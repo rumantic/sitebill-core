ffb1059143acb80ee35920a5bc95a4e0211f0c9a
Dmitriy Kondin <kondin@etown.ru>
2019-08-18 11:42:34 +0300

    - убираем лишний тест

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,19 +3,6 @@ use PHPUnit\Framework\TestCase;
 require_once('init_application.php');
 class StackTest extends TestCase
 {
-    public function testPushAndPop()
-    {
-        $stack = [];
-        $this->assertSame(0, count($stack));
-
-        array_push($stack, 'foo');
-        $this->assertSame('foo', $stack[count($stack)-1]);
-        $this->assertSame(1, count($stack));
-
-        $this->assertSame('foo', array_pop($stack));
-        $this->assertSame(0, count($stack));
-    }
-
     public function testGetAdminUserID () {
         $sitebill = SiteBill::sitebill_instance();
         $this->assertSame('1', $sitebill->getAdminUserId());

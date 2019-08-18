93e46827f411ff4d9abd3cc3ef671199deeb159b
Dmitriy Kondin <kondin@etown.ru>
2019-08-18 10:50:59 +0300

    - вычищаем код - тест конфига

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -19,6 +19,10 @@ class StackTest extends TestCase
     public function testGetAdminUserID () {
         $sitebill = SiteBill::sitebill_instance();
         $this->assertSame('1', $sitebill->getAdminUserId());
-
     }
+    public function testConfig () {
+        $sitebill = SiteBill::sitebill_instance();
+        $this->assertSame('rumantic.coder', $sitebill->getConfigValue('smtp1_login'));
+    }
+
 }

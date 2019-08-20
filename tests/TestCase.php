397a70ca1bc8bf5e26a7dbce40c0f9a147f00c0b
Dmitriy Kondin <kondin@etown.ru>
2019-08-20 19:49:09 +0300

    - добавил testDataModelLoader

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 use PHPUnit\Framework\TestCase;
 require_once('init_application.php');
 use system\lib\SiteBill;
+use system\lib\admin\data\Data_Manager;
 
 class StackTest extends TestCase
 {
@@ -12,6 +13,10 @@ class StackTest extends TestCase
     public function testConfig () {
         $sitebill = SiteBill::sitebill_instance();
         $this->assertSame('rumantic.coder', $sitebill->getConfigValue('smtp1_login'));
+    }
+    public function testDataModelLoader () {
+        $data_manager = new Data_Manager();
+        $this->assertSame('primary_key', $data_manager->data_model['data']['id']['type']);
     }
 
 }
